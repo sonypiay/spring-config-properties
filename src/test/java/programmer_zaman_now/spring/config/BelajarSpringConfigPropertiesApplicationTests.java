3f6f3b3675d1bc483eed53cdc95e31f218ec024f
sonypiay <pyscho30@gmail.com>
2025-06-21 20:53:03 +0700

    init project

---
+++ src/test/java/programmer_zaman_now/spring/config/BelajarSpringConfigPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package programmer_zaman_now.spring.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringConfigPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
